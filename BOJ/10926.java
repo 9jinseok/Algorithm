8dcc7c3cb4fc1f9e9e425b4200e457e654f58451
9jinseok <mer0120@naver.com>
2022-12-30 20:29:36 +0900

    BOJ:: 10926.java 추가

---
+++ BOJ/10926.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main_10926 {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.write(br.readLine() + "??!");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	
+}
