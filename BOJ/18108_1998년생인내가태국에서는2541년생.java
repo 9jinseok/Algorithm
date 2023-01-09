b6f85b90b2b64806cab2d64315a2b12bd6786aae
9jinseok <mer0120@naver.com>
2023-01-09 16:02:56 +0900

    BOJ:: 18108_1998년생인내가태국에서는2541년생.java 추가

---
+++ BOJ/18108_1998년생인내가태국에서는2541년생.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main_18108_1998년생인내가태국에서는2541년생{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x - 543);
+    }
+}
