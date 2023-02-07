cd60766d1253c6a929afa412a9201d19189b376d
9jinseok <mer0120@naver.com>
2023-02-07 19:16:39 +0900

    BOJ:: 25083_새싹.java 추가

---
+++ BOJ/25083_새싹.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class Main_25083_새싹 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        bw.write("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
