57998e84d0b8d72d5110627aca59ff4478fae7c8
9jinseok <mer0120@naver.com>
2023-03-18 16:32:28 +0900

    Softeer:: 주행거리.java 추가

---
+++ Softeer/주행거리.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_주행거리
+{
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        if(A > B)
+            System.out.println("A");
+        else if(B > A)
+            System.out.println("B");
+        else
+            System.out.println("same");
+
+        br.close();
+    }
+}
