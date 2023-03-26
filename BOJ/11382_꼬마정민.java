f741e6e64d2f959322ff3621986136d50478ce00
9jinseok <mer0120@naver.com>
2023-03-26 22:43:51 +0900

    BOJ:: 11382_꼬마정민.java 추가

---
+++ BOJ/11382_꼬마정민.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_11382_꼬마정민
+{
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(Long.parseLong(st.nextToken()) + Long.parseLong(st.nextToken()) + Long.parseLong(st.nextToken()));
+
+        br.close();
+    }
+}
