334a49c3c061d089c5d7e313b7a8388e7a621702
jinseok go <86669811+9jinseok@users.noreply.github.com>
2023-04-13 09:45:29 +0900

    BOJ:: 10026_적록색약.java 추가 (BFS)

---
+++ BOJ/10026_적록색약.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Main_10026_적록색약 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.close();
+    }
+}
